80dcb9f206aa652bd5918d1c169cd53aac01fe89
Devorah Posner <dleahposner@gmail.com>
2021-01-21 23:27:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,11 +27,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  words = ["wow", "arrays!", "am"]
-  words[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  words = ["wow", "am", "arrays!"]
-  words[-1]
+  array[-1]
 end
